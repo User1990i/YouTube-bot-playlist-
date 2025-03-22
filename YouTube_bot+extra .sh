682b1702f2a466f6a0714baa5df02dffae012cc0
User1990i <117085180+User1990i@users.noreply.github.com>
2025-03-22 20:49:18 +0200

    Update YouTube_bot+extra .sh

--- YouTube_bot+extra .sh
+++ YouTube_bot+extra .sh
@@ -62,9 +62,8 @@ show_banner() {
     echo -e "⠀⠀⠀⠀⠀⠘⣿⣷⣻⣿⢷⢶⢶⢶⢆⣗⡿⣇⣷⣿⡿⠀⠀⠀⠀⠀"
     echo -e "⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣛⣭⣭⣭⣭⣭⣻⣿⡿⠛⠀⠀⠀⠀⠀⠀"
     echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⠟⠛⠛⠛⠻⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "             YTBOT        "
     echo -e "" 
-    echo -e "${BOLD_RED}================================"
-    echo -e "        YouTube BOT         "
 }
 
 # Go Back function
