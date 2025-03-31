28c52abe3c458a82a9bab54c226eb5ff0e2b9d60
User1990i <117085180+User1990i@users.noreply.github.com>
2025-03-31 22:35:42 +0200

    Update YouTube_bot+extra .sh

--- YouTube_bot+extra .sh
+++ YouTube_bot+extra .sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # YouTube Downloader Bot - Version 1.9
-script_version="1.9"
+script_version="1.10"
 
 # Define output directories (No spaces in paths)
 base_dir="/storage/emulated/0/Music_Vids"
